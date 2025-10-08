e5b3257190ac989958617779ef545e1183ffe1d7
David Mak <david.18.19.21@gmail.com>
2025-10-08 14:28:43 +0800

    Initial Implementation for Wasm Backend (#341)
    Co-authored-by: Lionel Parreaux <lionel.parreaux@gmail.com>

--- hkmc2DiffTests/src/test/scala/hkmc2/MLsDiffMaker.scala
+++ hkmc2DiffTests/src/test/scala/hkmc2/MLsDiffMaker.scala
@@ -55,6 +55,11 @@ abstract class MLsDiffMaker extends DiffMaker:
   
   val typeCheck = FlagCommand(false, "typeCheck")
   
+  /**
+   * Enables Wasm support. All options in [[WasmDiffMaker]] are no-op if this option is not set.
+   */
+  val wasm = NullaryCommand("wasm")
+  
   
   // * Compiler configuration
   
@@ -89,7 +94,8 @@ abstract class MLsDiffMaker extends DiffMaker:
                 S(StackSafety(stackLimit = value))
         ,
       )),
-      liftDefns = Opt.when(liftDefns.isSet)(LiftDefns())
+      liftDefns = Opt.when(liftDefns.isSet)(LiftDefns()),
+      target = if wasm.isSet then CompilationTarget.Wasm else CompilationTarget.JS,
     )
   
   
